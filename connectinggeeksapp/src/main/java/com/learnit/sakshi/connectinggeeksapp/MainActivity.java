a6617acc698a16211f673df895f6ea6841745721
Sakshi Jain <sakshi7896@gmail.com>
2017-10-29 23:40:21 +0530

    Application Started

--- connectinggeeksapp/src/main/java/com/learnit/sakshi/connectinggeeksapp/MainActivity.java
+++ connectinggeeksapp/src/main/java/com/learnit/sakshi/connectinggeeksapp/MainActivity.java
@@ -2,6 +2,11 @@ package com.learnit.sakshi.connectinggeeksapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView t= (TextView)findViewById(R.id.textview);
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                t.setText("Button Pressed");
+            }
+        });
     }
 }
